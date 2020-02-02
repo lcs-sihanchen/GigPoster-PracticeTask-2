1ecd96a2bc3cfb0197629e74bbc1a08315d0f1a9
Chen Sihan <schen@lcs.on.ca>
2020-02-02 15:59:37 -0500

    background color

--- Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
@@ -33,7 +33,8 @@ let brown = Color(hue: 38, saturation: 99, brightness: 38, alpha: 100)
 let lightRed = Color(hue: 10, saturation: 80, brightness: 80, alpha: 75)
 
 // Begin your solution here...
-
+canvas.fillColor = yellow
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
 
 /*:
  ## Use Source Control
